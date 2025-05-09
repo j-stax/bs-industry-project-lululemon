4a4541ca9bcd9fdad3ea5f94293fbd88b1a92b41
j-stax <ahnjames01@gmail.com>
2025-05-09 19:14:00 -0400

    Stop showing SurveyNotice once user selects.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,37 @@ import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Survey from './pages/Survey/Survey'
+import SurveyNotice from './components/SurveyNotice/SurveyNotice'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [showSurveyNotice, setShowSurveyNotice] = useState()
+
+  useEffect(() => {
+    const storedNoticeAction = sessionStorage.getItem('showNotice')
+    if (storedNoticeAction === 'false') {
+      setShowSurveyNotice(false)
+    } else {
+      setShowSurveyNotice(true)
+    }
+  }, [])
+
+  const handleSurveyNoticeEnd = () => {
+    sessionStorage.setItem('showNotice', 'false')
+  }
+
+  // Add delete icon/function to SurveyNotice;
 
   return (
     <BrowserRouter>
         <div className="mobile-body">
+          {showSurveyNotice && 
+            <SurveyNotice handleSurveyNoticeEnd={handleSurveyNoticeEnd} />
+          }
           <Header />
           <main>
             <Routes>
-              <Route path="/" element={<Survey />} />
+              <Route path="/survey" element={<Survey />} />
               <Route path="/recommendation" element={<p>Recommendation</p>} />
             </Routes>
           </main>
