31bf0625924145c9572c8b6f03bd301a2282a5f5
Helios Cao <caxza1990@mgial.com>
2025-05-10 14:27:50 -0400

    added profile and stats for dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Survey from './pages/Survey/Survey'
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Survey from "./pages/Survey/Survey";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-
   return (
     <BrowserRouter>
-        <div className="mobile-body">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Survey />} />
-              <Route path="/recommendation" element={<p>Recommendation</p>} />
-            </Routes>
-          </main>
-        </div>
+      <div className="mobile-body">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Survey />} />
+            <Route path="/recommendation" element={<p>Recommendation</p>} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
