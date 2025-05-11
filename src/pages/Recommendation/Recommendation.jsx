6db4367db3e7ae2f3973c3d23eb1c02c71f5ed27
j-stax <ahnjames01@gmail.com>
2025-05-11 17:12:57 -0400

    Fix background color and margin/padding on Recommendation page.

--- src/pages/Recommendation/Recommendation.jsx
+++ src/pages/Recommendation/Recommendation.jsx
@@ -6,21 +6,23 @@ export default function Recommendation() {
     const event = events[1]
 
     return (
-        <div className="recommendation">
-            <Event 
-                imageUrl={event.imageUrl}
-                date={event.date}
-                location={event.location}
-                address={event.address}
-                time={event.time}
-            />
-            <div className="recommendation__para-container">
-                <p className="recommendation__para">
-                    Based on the survey, we think you would like this {event.activity} event that lululemon is sponsoring!
-                </p>
-                <p className="recommendation__para">
-                    Come and connect with the lululemon {event.activity} community. We hope to see you there! 
-                </p>
+        <div className="recommendation-wrapper">
+            <div className="recommendation">
+                <Event 
+                    imageUrl={event.imageUrl}
+                    date={event.date}
+                    location={event.location}
+                    address={event.address}
+                    time={event.time}
+                />
+                <div className="recommendation__para-container">
+                    <p className="recommendation__para">
+                        Based on the survey, we think you would like this {event.activity} event that lululemon is sponsoring!
+                    </p>
+                    <p className="recommendation__para">
+                        Come and connect with the lululemon {event.activity} community. We hope to see you there! 
+                    </p>
+                </div>
             </div>
         </div>
     )
