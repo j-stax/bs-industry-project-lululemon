da8be2427144dc643d87774808fd6c9258d28648
j-stax <ahnjames01@gmail.com>
2025-05-12 22:24:47 -0400

    SurveyNotice.jsx - handle close button

--- src/components/SurveyNotice/SurveyNotice.jsx
+++ src/components/SurveyNotice/SurveyNotice.jsx
@@ -6,14 +6,18 @@ import { useRef } from 'react'
 export default function SurveyNotice({ handleSurveyNoticeEnd }) {
     const noticeRef = useRef(null)
 
-    const handleClick = (e) => {
+    const handleClick = () => {
         noticeRef.current.classList.add("hidden")
         handleSurveyNoticeEnd()
     }
 
+    const handleClose = () => {
+        noticeRef.current.classList.add("hidden")
+    }
+
     return (
         <div ref={noticeRef} className="notice">
-            <IoIosClose className="notice__close-icon" size={24} />
+            <IoIosClose className="notice__close-icon" size={24} onClick={handleClose} />
             <p className="notice__para">
                 Complete a quick survey and receive 10% off your next purchase!&nbsp; 
                 <Link to="/survey" 
